62c44bfa9f0dc521869411c8ac1c3b00c143515a
SALOME <salome.rsb@gmail.com>
2024-04-01 18:49:28 -0300

    agrego el puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main (){
 
-    printf ("Hola mundo");
-   return 0 ;
+    printf ("Hola mundo\n");
+    int *puntero, numero=8;
+    puntero=&numero;
+    printf("el contenido de lo que apunta el puntero %d\n", *puntero);
+    printf("lo que contine el puntero %p\n", puntero);
+    printf("la direccion de memoria de la variable numero %p\n",  &numero);
+    printf(" lo que contiene de dato la variable nuemro %d\n", numero);
+    int tamanio = sizeof(numero);
+    printf("El tamaño de la variable numero es: %d bytes\n", tamanio);
+       return 0 ;
 }
